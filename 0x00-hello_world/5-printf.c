7ae1bd4c8748947c2fe785951775189d464a0d80
Paul Tidings Steiner <adjeisteiner@gmail.com>
2022-09-08 11:03:33 -0700

    Pushing first C programming files

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * 
+ * main - Function starting point
+ * 
+ * Return: 0
+ *
+ */
+
+int main()
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
