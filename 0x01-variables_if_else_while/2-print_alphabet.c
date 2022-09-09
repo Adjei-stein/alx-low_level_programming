cdc67cee94b7073bebf1bc79378b36d805ea8bff
Paul Tidings Steiner <adjeisteiner@gmail.com>
2022-09-09 15:56:22 -0700

    Second Work

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - The basic function
+ *
+ * Return: 0
+ *
+ */
+int main(void)
+{
+	char alpha;
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+	{
+	putchar(alpha);
+	}
+	putchar('\n');
+	return (0);
+}
