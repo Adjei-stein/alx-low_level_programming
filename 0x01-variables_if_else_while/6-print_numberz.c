958c81a48022e24afcfa29bac5a9209ff3a29562
Paul Tidings Steiner <adjeisteiner@gmail.com>
2022-09-09 16:13:19 -0700

    Second Work

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - The basic function
+ *
+ * Return: 0
+ * 
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+	putchar((n % 10) + '0');
+	}
+	putchar('\n');
+	return (0);
+}
