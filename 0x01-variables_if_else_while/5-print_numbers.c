c39add7de606788c38431aab41b73854a604f926
Paul Tidings Steiner <adjeisteiner@gmail.com>
2022-09-09 16:10:03 -0700

    Second Work

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - The basic function
+ *
+ * Return: 0
+ *
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+	printf("%d", n);
+	}
+	printf("\n");
+	return (0);
+}
