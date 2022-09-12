44e275216ef027d8ec0af2deee8e5bf5511f3bab
Paul Tidings Steiner <adjeisteiner@gmail.com>
2022-09-12 05:14:44 -0700

    My first README

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,57 @@
+nclude <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - The basic function
+ * 
+ * Return: 0
+ * 
+ * */
+int main(void)
+{
+	int n;
+	int k;
+	int j;
+	int r;
+	int p;
+
+	j = 1;
+	       
+	for (n = 0; n < 9; n++)
+	{
+	p = 1;
+	for (k = 0; k < 9; k++)
+	{
+	if (k < j) 
+	{
+	continue;   
+																				        	}	
+	if (n == 8 && k == 9)
+	{
+	continue;
+																				                }
+
+	for (r = 0; r <= 9; r++)
+	{
+																						if (r <= p) 
+																						{
+																						continue;   
+																						}
+																						putchar((n % 10) + '0'); 
+																						putchar((k % 10) + '0');
+																						putchar ((r % 10)+ '0');
+																						if (k == 8 && r == 9)
+																						{
+																						continue;
+																						}
+																						putchar(',');
+																						putchar(' ');
+																						}			                            
+	p++;         
+	}
+	j++;                
+	}
+	putchar('\n');      
+	 return (0);
+	}
