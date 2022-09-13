f01b0b9c1d213f8be5a1bcb987e712a87a735eca
Paul Tidings Steiner <adjeisteiner@gmail.com>
2022-09-13 10:02:38 -0700

    hmm

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - That's my function, Baby
+ *
+ * Return: 0
+ *
+ */
+void print_alphabet_x10(void)
+{
+	int strt;
+	int n;
+	for (n = 1; n <= 10; n++)
+	{
+	for (strt = 97; strt < 123; strt++)
+	{
+	_putchar(strt);
+	}
+	}
+	_putchar('\n');
+}
