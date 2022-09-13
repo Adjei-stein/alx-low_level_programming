84e9e20c48c55f4a90bd05d80452c7e236d4061b
Paul Tidings Steiner <adjeisteiner@gmail.com>
2022-09-13 09:20:36 -0700

    hmm

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *
+ * main - That's my function, Baby
+ *
+ * Return: 0
+ *
+ */
+int main(void)
+{
+	int strt;
+
+	for (strt = 97; strt < 123; strt++)
+	{
+	_putchar(strt);
+	}
+	_putchar('\n');
+	return (0);
+}
