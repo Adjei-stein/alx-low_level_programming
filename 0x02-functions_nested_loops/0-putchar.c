a4740aef626dc0fc8a99ef5995e3c7d87d910663
Paul Tidings Steiner <adjeisteiner@gmail.com>
2022-09-13 05:10:14 -0700

    hmm

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include <_putchar.c>
+/**
+ * main - The basic function
+ * 
+ * Return: 0
+ * 
+ */
+int main(void)
+{
+	_putchar()
+	putchar(\n)
+	return (0)
+}
