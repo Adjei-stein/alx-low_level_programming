cb1fb7b700a4ef653bba7556e786562848626f4f
Paul Tidings Steiner <adjeisteiner@gmail.com>
2022-09-15 11:09:18 -0700

    tueh

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -14,23 +14,20 @@ int main(void)
 		if ((n % 3 == 0) && (n % 5 == 0)
 		{
 			printf("FizzBuzz");
-			putchar(' ');
 		}
 		else if (n % 3 == 0)
 		{
 			printf("Fizz");
-			putchar(' ');
 		}
 		else if (n % 5 == 0)
 		{
 			printf("Buzz");
-			putchar(' ');
 		}
 		else
 		{
 			printf("%d", n);
-			putchar(' ');
 		}
+		printf(" ")
 	}
 	printf("\n");
 	return (0);
