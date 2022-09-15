5771be4b5b3d7fc06b612e26139fef81d4adb3b3
Paul Tidings Steiner <adjeisteiner@gmail.com>
2022-09-15 09:23:35 -0700

    Tueh

---
+++ 0-isupper.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _isupper - check the code.
+ *
+ * Return: Always 0.
+ */
+int _isupper(void)
+{
+	int r;
+	int s;
+
+	r = 1;
+	s = 0;
+	if (c == 'A')
+	{
+		printf("%d", r);
+	}
+	else if (c == 'a')
+	{
+		printf("%d", s);
+	}
+	return (0);
+}
