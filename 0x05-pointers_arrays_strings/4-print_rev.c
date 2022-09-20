b400966ee3aa015a0b7e270d8e240acb0f288c9f
Paul Tidings Steiner <adjeisteiner@gmail.com>
2022-09-20 05:08:41 -0700

    05_work

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * print_rev - My function, hehe
+ * @str: Char to be received
+ * Return: 0;
+ */
+int print_rev(char *str)
+{
+	int i;
+	int c;
+	int b;
+
+	i = 0;
+	char *ar = str;
+	while (*ar != '\0')
+	{
+	i++;
+	ar++;
+	}
+	c = i;
+	for (b = -1; b >= (-c); b--)
+	{
+		printf("%c", ar[b]);
+	}
+	printf("\n");
+	return (0);
+}
---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * rev_string - That's my function
+ * @str:vThis is the received variable
+ * Return: 0
+ */
+int rev_string(char *str)
+{
+	int i;
+	int c;
+	int b;
+
+	i = 0;
+	char *ar = str;
+	while (*ar != '\0')
+	{
+	i++;
+	ar++;
+	}
+	c = i;
+	/*printf("%d\n", c);*/
+	for (b = -1; b >= (-c); b--)
+	{
+	printf("%c", ar[b]);
+	}
+	printf("\n");
+	return (0);
+}
